e4d1acf18a3e4de70eac9845a12e0041003b6d96
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2025-04-16 22:50:21 +0530

    Added Textinput component

---
+++ src/UI/Inputs/TextInput/TextInput.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent, InputHTMLAttributes } from 'react';
+import Label from '../Label';
+import styles from './TextInput.module.css';
+
+//interface TextInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onValueChange: (value: string) => void;
+  label?: string;
+  id?: string;
+  required?: boolean;
+}
+
+const TextInput: React.FC<TextInputProps> = ({ 
+  onChange,
+  onValueChange,
+  label,
+  id,
+  required,
+  ...props
+ }) => {
+  const inputId = id || `textinput-${Math.random().toString(36).slice(2)}`;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onValueChange?.(e.target.value);
+    onChange?.(e);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {label && (
+        <Label htmlFor={inputId} required={required}>
+          {label}
+        </Label>
+      )}
+      <input
+        id={inputId}
+        className={styles.input}
+        type="text"
+        onChange={handleChange}
+        required={required}
+        {...props}
+      />
+    </div>
+  )
+};
+
+export default TextInput;
