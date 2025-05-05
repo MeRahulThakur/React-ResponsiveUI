45bd796c858d4e4056e307f7196f4fc0d87206f2
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2025-05-05 14:12:04 +0530

    Snackbar dynamic position implementation

--- src/pages/DesignSystemPage.tsx
+++ src/pages/DesignSystemPage.tsx
@@ -3,16 +3,41 @@ import Accordion from "../UI/Accordion/Accordion";
 import Button from "../UI/Button";
 import Modal from "../UI/Modal";
 import SnackBar from "../UI/SnackBar";
+import GroupedRadio from "../UI/Inputs/Group/GroupedRadio";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+const radioOptions: Option[] = [
+  { label: 'Top Left', value: 'top-left' },
+  { label: 'Top Center', value: 'top-center' },
+  { label: 'Top Right', value: 'top-right' },
+  { label: 'Bottom Left', value: 'bottom-left' },
+  { label: 'Bottom Center', value: 'bottom-center' },
+  { label: 'Bottom Right', value: 'bottom-right' },
+];
 
 const DesignSystemPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [snackOpen, setSnackOpen] = useState(false);
+  const [anchorOrigin, setAnchorOrigin] = useState<'top-left' | 'top-center' | 'top-right' | 'bottom-left' | 'bottom-center' | 'bottom-right'>('bottom-left');
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   const handleConfirm = () => {
     alert('Confirmed!');
     closeModal();
   };
+  const handleAnchorOriginChange = (selected: Option|null) => {
+    const allowedValues = [
+      'top-left', 'top-center', 'top-right',
+      'bottom-left', 'bottom-center', 'bottom-right'
+    ] as const;
+    if (selected && allowedValues.includes(selected.value as typeof allowedValues[number])) {
+      setAnchorOrigin(selected.value as typeof allowedValues[number]);
+    }
+  }
 
   return (
     <div className="page">
@@ -46,17 +71,24 @@ const DesignSystemPage = () => {
       </div>
       <h2>SnackBar</h2>
       <div style={{ display: 'flex', gap: '1rem' }}>
-      <Button variant="outlined" color="default" onClick={()=>setSnackOpen(true)}>Show Snackbar</Button>
-      <SnackBar
-        open={snackOpen}
-        onClose={() => setSnackOpen(false)}
-        //message="Form saved successfully!"
-        variant="error"
-        anchorOrigin="top-left"
-        autoHideDuration={3000}
-      >
-        <strong>Error:</strong> Something went wrong!
-      </SnackBar>
+        <GroupedRadio
+          id="anchorOrigin"
+          orientation="row"
+          options={radioOptions}
+          defaultSelected={'bottom-left'}
+          onChange={handleAnchorOriginChange}
+        />
+        <Button variant="outlined" color="default" onClick={() => setSnackOpen(true)}>Show Snackbar</Button>
+        <SnackBar
+          open={snackOpen}
+          onClose={() => setSnackOpen(false)}
+          //message="Form saved successfully!"
+          variant="error"
+          anchorOrigin={anchorOrigin}
+          autoHideDuration={3000}
+        >
+          <strong>Error:</strong> Something went wrong!
+        </SnackBar>
       </div>
     </div>
   )
